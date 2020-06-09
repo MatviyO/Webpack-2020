48389c1b1eab91a022dad64b050b3b2fb98018c9
MatviyO <33325458+MatviyO@users.noreply.github.com>
2020-06-09 22:44:55 +0300

    css

--- src/index.js
+++ src/index.js
@@ -1,2 +1,10 @@
 import Post from "./Post";
-const post = new Post('webpack post title')
+import './styless/styles.css'
+// import json from'./assets/json.json'
+// import xml from './assets/data.xml'
+import Webpacklogo from './assets/webpack-logo.png'
+
+const post = new Post('webpack post title', Webpacklogo)
+console.log('post',post)
+// console.log(json, 'json')
+// console.log(xml, 'xml')
--- webpack.config.js
+++ webpack.config.js
@@ -1,12 +1,44 @@
+
 const path = require('path');
+const HTMLWebpackPlugin = require('html-webpack-plugin')
+const {CleanWebpackPlugin} =  require('clean-webpack-plugin')
+
 module.exports = {
+    context: path.resolve(__dirname,'src'),
     mode: 'development',
     entry: {
-        main:'./src/index.js',
-    analitics: './src/analitics.js'},
+        main:'./index.js',
+    analitics: './analitics.js'},
     output: {
-        filename: '[name].bundle.[chunkhash].js',
+        filename: '[name].[contenthash].js',
         path: path.resolve(__dirname, 'dist')
 
+    },
+    plugins: [
+        new HTMLWebpackPlugin({
+            template: './index.html'
+        }),
+        new CleanWebpackPlugin()
+    ],
+    module: {
+        rules: [
+            {
+                test: /\.css$/,
+                use: ['style-loader','css-loader']
+            },
+            {
+                test: /\.(png|jpg|svg|gif)$/,
+                use: ['file-loader']
+            },
+            {
+                test: /\.(ttf|woff|woff2|eot)$/,
+                use: ['file-loader']
+            },
+            {
+                test: /\.xml$/,
+                use: ['xml-loader']
+            }
+
+        ]
     }
 }
