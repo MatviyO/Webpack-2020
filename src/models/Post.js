bc22630202609b13825871848cbc8ba9de337496
MatviyO <33325458+MatviyO@users.noreply.github.com>
2020-06-10 21:25:38 +0300

    full

--- src/models/Post.js
+++ src/models/Post.js
@@ -1,14 +1,19 @@
 export default class Post {
-    constructor(title, img) {
-        this.title = title
-        this.img = img
-        this.date = new Date()
-    }
-    toSting() {
-       return JSON.stringify({
-            title: this.title,
-            date: this.date.toJSON(),
-            img: this.img
-        }, null, 2)
-    }
+  constructor(title, img) {
+    this.title = title
+    this.img = img
+    this.date = new Date()
+  }
+
+  toString() {
+    return JSON.stringify({
+      title: this.title,
+      date: this.date.toJSON(),
+      img: this.img
+    }, null, 2)
+  }
+
+  get uppercaseTitle() {
+    return this.title.toUpperCase()
+  }
 }
